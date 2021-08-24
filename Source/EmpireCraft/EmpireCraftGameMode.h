d99fb4f29a5ba197670bd0f744ddcfee4ff787c7
Your Name <you@example.com>
2021-08-24 09:35:48 +0800

    ThirdPerson original version

---
+++ Source/EmpireCraft/EmpireCraftGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EmpireCraftGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEmpireCraftGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEmpireCraftGameMode();
+};
+
+
+
